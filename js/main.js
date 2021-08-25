e39a55e7cb80f3a6dab5a59e65ded9b9067e4318
Никита Рабчук <nikita.rabchuk@gmail.com>
2021-08-25 15:24:31 +0300

    fix

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// let scrollPos = window.pageYOffset;
+// let a;
+
+// document.addEventListener("scroll", event => {
+// 	if (window.pageYOffset > scrollPos) {
+// 		document.body.classList.add("scrollDown");
+// 		document.body.classList.remove("scrollUp");
+// 	} else if (window.pageYOffset < scrollPos) {
+// 		document.body.classList.add("scrollUp");
+// 		document.body.classList.remove("scrollDown");
+// 	}
+// 	scrollPos = window.pageYOffset;
+// });
