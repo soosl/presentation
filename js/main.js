0016f8a9bab0c9a171957dba5a1161a529b83141
Никита Рабчук <nikita.rabchuk@gmail.com>
2021-08-26 16:22:38 +0300

    Сделан Желе при скролле

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-// let scrollPos = window.pageYOffset;
-// let a;
+const section = document.querySelector('.parallax-effect');
 
-// document.addEventListener("scroll", event => {
-// 	if (window.pageYOffset > scrollPos) {
-// 		document.body.classList.add("scrollDown");
-// 		document.body.classList.remove("scrollUp");
-// 	} else if (window.pageYOffset < scrollPos) {
-// 		document.body.classList.add("scrollUp");
-// 		document.body.classList.remove("scrollDown");
-// 	}
-// 	scrollPos = window.pageYOffset;
-// });
+let currentPos = window.pageYOffset;
+
+const update = () => {
+	const newPos = window.pageYOffset;
+	const diff = newPos - currentPos;
+	let speed = diff * 0.15 > 5 ? 5 : diff * 0.15;
+
+
+	
+	section.style.transform = `skewY(${ speed }deg)`;
+	
+	currentPos = newPos;
+	
+	requestAnimationFrame(update);
+}
+
+update();
